7a17f66516e18e0159986bcd37f5861e7bd09bf5
afekTheMiniLearner <afeksa2000@gmail.com>
2023-01-21 22:24:11 +0200

    digits comments

--- src/base-components/Digit/Digit.jsx
+++ src/base-components/Digit/Digit.jsx
@@ -3,18 +3,7 @@ import PropTypes from "prop-types";
 import "./Digit.scss";
 import { activeLinesByNumber, trapezoids } from "../../consts";
 
-/* export const activeLines = {
-  0: [1, 2, 3, 5, 6, 7],
-  1: [3, 6],
-  2: [1, 3, 4, 5, 7],
-  3: [1, 3, 4, 6, 7],
-  4: [2, 3, 4, 6],
-  5: [1, 2, 4, 6, 7],
-  6: [1, 2, 4, 5, 6, 7],
-  7: [1, 3, 6],
-  8: [1, 2, 3, 4, 5, 6, 7],
-  9: [1, 2, 3, 4, 6],
-}; */
+
 export function Digit({ value }) {
   return (
     <div className="digitsContainer">
@@ -23,7 +12,7 @@ export function Digit({ value }) {
 
         return (
           <div
-            id={`d${divNumber}`}
+            id={`d${divNumber}`} // change to class
             key={index}
             className={isActive ? "trapezoid" : "trapezoid passive"}
           ></div>
