6cbcc4c77bb6bc53b30577a8fb421d7f374ff8f1
afekTheMiniLearner <afeksa2000@gmail.com>
2023-01-25 20:35:37 +0200

    now every time unit presented with 2 digits, even if its only "7" for example it will be represented as "07", my name is bond, james bond

--- src/components/DigitsClock/DigitsClock.jsx
+++ src/components/DigitsClock/DigitsClock.jsx
@@ -3,54 +3,72 @@ import PropTypes from "prop-types";
 import { Digit } from "../../base-components";
 import "./DigitsClock.scss";
 
-export function DigitsClock({ updateAmPm ,ampmState,localeTime,setLocaleTime,currentDay,setCurrentDay}) {  
-  const [day,time, ampm] = localeTime.split(' ')
-  const [hh, mm, ss] = time.split(':'); 
+export function DigitsClock({
+  updateAmPm,
+  ampmState,
+  localeTime,
+  setLocaleTime,
+  currentDay,
+  setCurrentDay,
+}) {
+  const [day, time, ampm] = localeTime.split(" ");
+  let [hh, mm, ss] = time.split(":");
 
-  useEffect(()=>{
-    const timerInterval = setInterval(()=>{
-      setLocaleTime(new Date().toLocaleDateString('en-US', { 
-        weekday: 'short', 
-        hour: 'numeric', 
-        minute: 'numeric', 
-        second: 'numeric' 
-      }));
-    },1000);
+  hh = hh.padStart(2, "0");
+  mm = mm.padStart(2, "0");
+  ss = ss.padStart(2, "0");
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setLocaleTime(
+        new Date().toLocaleDateString("en-US", {
+          weekday: "short",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        })
+      );
+    }, 1000);
 
     return () => clearInterval(timerInterval);
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    if(day !== currentDay){
-      setCurrentDay(day)
+  useEffect(() => {
+    if (day !== currentDay) {
+      setCurrentDay(day);
     }
-  },[day])
+  }, [day]);
 
-  useEffect(()=>{
-    if(ampm !== ampmState){
-      updateAmPm(ampm)
+  useEffect(() => {
+    if (ampm !== ampmState) {
+      updateAmPm(ampm);
     }
-  },[])
-  
+  }, []);
+
   return (
     <div className="numbersContainer">
-      
-       {hh.split('').map((digit,index)=><Digit value={+digit} key={`hh ${index}`} />)} 
-       <div className="twoDots">:</div> 
-       {mm.split('').map((digit,index)=><Digit value={+digit} key={`mm ${index}`}/>)} 
-       <div className="twoDots">:</div> 
-       {ss.split('').map((digit,index)=><Digit value={+digit} key={`ss ${index}`}/>)} 
-      </div>
+      {hh.split("").map((digit, index) => (
+        <Digit value={+digit} key={`hh ${index}`} />
+      ))}
+      <div className="twoDots">:</div>
+      {mm.split("").map((digit, index) => (
+        <Digit value={+digit} key={`mm ${index}`} />
+      ))}
+      <div className="twoDots">:</div>
+      {ss.split("").map((digit, index) => (
+        <Digit value={+digit} key={`ss ${index}`} />
+      ))}
+    </div>
   );
 }
 
 DigitsClock.propTypes = {
   updateAmPm: PropTypes.func,
   ampmState: PropTypes.string,
-  localeTime:PropTypes.string,
-  setLocaleTime:PropTypes.func,
-  currentDay:PropTypes.string,
-  setCurrentDay:PropTypes.func,
+  localeTime: PropTypes.string,
+  setLocaleTime: PropTypes.func,
+  currentDay: PropTypes.string,
+  setCurrentDay: PropTypes.func,
 };
 
 //DayText.defaultProps = {};
