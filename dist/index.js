fa3098bb26dc1f778c7edd9a61283a1ca9e914ae
afekTheMiniLearner <afeksa2000@gmail.com>
2023-03-05 20:13:16 +0200

    updating version

--- dist/index.js
+++ dist/index.js
@@ -12,12 +12,22 @@ Object.defineProperty(exports, "Clock", {
 var _react = _interopRequireDefault(require("react"));
 var _client = _interopRequireDefault(require("react-dom/client"));
 require("./index.css");
-var _app = _interopRequireDefault(require("./app"));
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
 var _components = require("./components");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var root = _client.default.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_app.default, null)));
+/* import App from "./app";
+
+* Hidden to prevent multiple calling ReactDOMClient.createRoot() 
+in case someone using this as NPM package
+unhide if you want to run the app ! 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 import { Clock } from "./components";
+/* import App from "./app";
+
+* Hidden to prevent multiple calling ReactDOMClient.createRoot() 
+in case someone using this as NPM package
+unhide if you want to run the app ! 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
