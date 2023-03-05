56c2e4afa1cf67a13fd0a0044651528cc23f0128
afekTheMiniLearner <afeksa2000@gmail.com>
2023-03-05 11:30:37 +0200

    new version

--- dist/index.js
+++ dist/index.js
@@ -5,17 +5,9 @@ var _client = _interopRequireDefault(require("react-dom/client"));
 require("./index.css");
 var _app = _interopRequireDefault(require("./app"));
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var root = _client.default.createRoot(document.getElementById("root"));
-root.render(
-  /*#__PURE__*/ _react.default.createElement(
-    _react.default.StrictMode,
-    null,
-    /*#__PURE__*/ _react.default.createElement(_app.default, null)
-  )
-);
+root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_app.default, null)));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
