acdb3f99cbec3ee6d5ae72487ed138f5248ed04c
afekTheMiniLearner <afeksa2000@gmail.com>
2023-03-10 23:08:21 +0200

    the app modified to show the area clocks in a row and not col

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 import { Clock } from "./components";
 import { TimeAreaText } from "./base-components";
-import { TIME_ZONES } from "./utils";
+import { TIME_ZONES, getZoneTimestamp } from "./utils";
 
 function App() {
   return (
     <div className="app-container">
-      <div className="clocks-area-container">
-        {TIME_ZONES.map((zone, i) => {
-          return (
-            <div key={i} className={`clock-and-zone-container`}>
-              <TimeAreaText label={zone.name} />
-              <Clock />
-            </div>
-          );
-        })}
-      </div>
+      {TIME_ZONES.map((zone, i) => {
+        return (
+          <div key={i} className={`clock-and-zone-container`}>
+            <TimeAreaText label={zone.name} />
+            <Clock timestamp={getZoneTimestamp(zone)} />
+          </div>
+        );
+      })}
     </div>
   );
 }
