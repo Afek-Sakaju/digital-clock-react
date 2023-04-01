45e3941c0de69385a4ac69d36a87b6d1336e06f7
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-01 17:21:02 +0300

    .

--- dist/index.js
+++ dist/index.js
@@ -9,4 +9,12 @@ Object.defineProperty(exports, "Clock", {
     return _components.Clock;
   }
 });
+var _react = _interopRequireDefault(require("react"));
+var _client = require("react-dom/client");
+var _App = _interopRequireDefault(require("./App"));
+require("./index.css");
 var _components = require("./components");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var rootElement = document.getElementById("root");
+var root = (0, _client.createRoot)(rootElement);
+root.render( /*#__PURE__*/_react.default.createElement(_App.default, null));
