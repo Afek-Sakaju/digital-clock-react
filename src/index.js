df35bfc8db041a00db816a3e3166b5d5e90ddf6d
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-01 15:25:23 +0300

    new version with updated readme and updated react version 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-import "./index.css";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
-
 export { Clock } from "./components";
