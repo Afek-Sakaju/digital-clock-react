1146201183d988c62f67f517be1fcb49bf5b53bb
afekTheMiniLearner <afeksa2000@gmail.com>
2023-04-07 18:52:56 +0300

    publish new npm version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./app";
-
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
-
 // eslint-disable-next-line import/prefer-default-export
 export { Clock } from "./components";
