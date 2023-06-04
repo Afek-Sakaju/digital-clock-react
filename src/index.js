bfe133341832ce35fb58551506a784e27af4fc5f
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-04 18:17:07 +0300

    removed user-select from certain text elements and released new version

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
-export { Clock } from "./components";
+// export { Clock } from "./components";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./app";
+import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
