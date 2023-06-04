4b079927f77d8ecbae64f94fcb608a3a88198f09
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-04 18:18:48 +0300

    deleted index js content expect the export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-// export { Clock } from "./components";
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./app";
-import "./index.scss";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export { Clock } from "./components";
