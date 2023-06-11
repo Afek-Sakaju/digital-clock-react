43d7bf78d45952d7635505058804a9707ce9a4f5
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-11 16:54:16 +0300

    changed clock's shadow and removed the border

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// eslint-disable-next-line import/prefer-default-export
-export { Clock } from "./components";
+// // eslint-disable-next-line import/prefer-default-export
+// export { Clock } from "./components";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.scss";
+import App from "./app";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
