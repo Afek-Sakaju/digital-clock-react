df63db931645db708fcb49f0980fb38439e52bbb
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-11 17:35:24 +0300

    published new version with the design changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-// // eslint-disable-next-line import/prefer-default-export
-// export { Clock } from "./components";
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import "./index.scss";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// eslint-disable-next-line import/prefer-default-export
+export { Clock } from "./components";
