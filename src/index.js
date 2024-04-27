4d22a882802c409ebfbf390d22237adb7cbfd781
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-04-27 13:43:18 +0300

    Refactor index.js and package.json files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+// * Production
 // eslint-disable-next-line import/prefer-default-export
 export { Clock } from "./components";
+
+// * Development
+/*
+ import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+*/
